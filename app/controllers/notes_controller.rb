80a087b002e9efe007880dd796f533fff1fe13e3
derrick b <dbozich1@gmail.com>
2018-09-02 22:37:23 +0000

    automatically backed up by learn

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,7 +6,7 @@ class NotesController < ApplicationController
   def create
     if logged_in?
       @note = Note.new(:content => params[:note][:content], :user_id => session[:user_id], :visible_to => params[:note][:visible_to])
-    
+      @note = Note.new(note_params)
       if @note.save
         redirect_to '/'
       else
