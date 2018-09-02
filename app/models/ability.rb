d656d0a9b2c40b023ff9f2cc7c40636255faaf02
derrick b <dbozich1@gmail.com>
2018-09-02 23:26:33 +0000

    automatically backed up by learn

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,14 +2,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    binding.pry
-    # if user.admin?
-    #   can :manage, :all
-    # else
-    #   can :read, :all
-    # end
-
-
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
